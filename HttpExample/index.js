5e3daa6244d886da4fe8eb33b6281c5b147bb51e
thomas-kreuscher <thomas.david.kreuscher@gmail.com>
2025-12-07 19:40:25 +0100

    2. änderung

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,13 +1,14 @@
 module.exports = async function (context, req) {
     context.log('HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
+    const name = req.query.name || (req.body && req.body.name);
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        ? `Hello, ${name}. This HTTP triggered function executed successfully.`
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body.";
 
     context.res = {
         status: 200,
         body: responseMessage
     };
 };
+
