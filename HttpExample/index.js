ab6234300d76b826051023457915c0781627407f
thomas-kreuscher <thomas.david.kreuscher@gmail.com>
2025-12-07 22:43:19 +0100

    #3

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,14 +1,23 @@
 module.exports = async function (context, req) {
-    context.log('HTTP trigger function processed a request.');
+    context.log('Function triggered');
 
-    const name = req.query.name || (req.body && req.body.name);
-    const responseMessage = name
-        ? `Hello, ${name}. This HTTP triggered function executed successfully.`
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body.";
+    try {
+        const name = req.query.name || (req.body && req.body.name);
+        const responseMessage = name
+            ? `Hello, ${name}. This HTTP triggered function executed successfully.`
+            : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body.";
 
-    context.res = {
-        status: 200,
-        body: responseMessage
-    };
+        context.res = {
+            status: 200,
+            body: responseMessage
+        };
+    } catch (err) {
+        context.log.error("Function error:", err);
+        context.res = {
+            status: 500,
+            body: "Internal Server Error"
+        };
+    }
 };
+
 
